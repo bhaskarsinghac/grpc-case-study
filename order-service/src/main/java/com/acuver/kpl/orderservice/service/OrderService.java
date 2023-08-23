e3e189d3dc1243628c1651ef0fc323e8214c8cc6
Bhaskar Singh <110682303+bhaskarsinghac@users.noreply.github.com>
2023-08-23 11:14:55 +0530

    refactor to reactive

--- order-service/src/main/java/com/acuver/kpl/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/acuver/kpl/orderservice/service/OrderService.java
@@ -6,17 +6,23 @@ import com.acuver.kpl.orderservice.model.Order;
 import com.acuver.kpl.orderservice.repository.OrderRepository;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoSink;
 
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -43,7 +49,7 @@ public class OrderService {
         }
         var invReserveReq = invReserveReqBuilder.build();
 
-        var invReserveResFuture = futureStub
+        ListenableFuture<ReserveInventoryListResponse> invReserveResFuture = futureStub
                 .withDeadlineAfter(25, TimeUnit.SECONDS).reserveInventory(invReserveReq);
 
         return Mono.create(monoSink -> Futures.addCallback(invReserveResFuture, new FutureCallback<>() {
@@ -51,53 +57,78 @@ public class OrderService {
             @Override
             public void onSuccess(@NullableDecl ReserveInventoryListResponse invReserveRes) {
                 log.warn("Inside Success");
-                AtomicInteger reservedCounter = new AtomicInteger();
-                try {
-                    Mono.just(createOrderReq).doOnSuccess(order -> {
-                                for (Order.OrderLine orderLine : order.getOrderLineEntries()) {
-                                    for (ReserveInventoryResponse response : invReserveRes.getInvResponseList()) {
-                                        if (orderLine.getSku().equals(response.getProductId())) {
-                                            if (response.getSuccess()) {
-                                                orderLine.setStatus("RESERVED");
-                                                reservedCounter.addAndGet(1);
-                                            } else {
-                                                orderLine.setStatus("CANCELLED");
-                                                orderLine.setCancellationReason("Insufficient Inventory");
-                                            }
-                                            break;
-                                        }
-                                    }
-                                }
-//                                orderRepository.save(order).subscribe();
-                            }).doOnError(throwable -> log.error(throwable.getMessage()))
-                            .subscribe();
 
-                    CreateOrderResponse res = new CreateOrderResponse();
-                    res.setSellerOrderId(createOrderReq.getSellerOrderId());
-                    if (reservedCounter.get() == createOrderReq.getOrderLineEntries().size()) {
-                        res.setOrderStatus("CONFIRMED");
-                        res.setCancellationReason(null);
-                        monoSink.success(res);
-                        createOrderReq.setStatus("CONFIRMED");
-                        orderRepository.save(createOrderReq).subscribe();
-                    } else {
-                        res.setOrderStatus("CANCELLED");
-                        res.setCancellationReason("Unable to reserve inventory");
-                        monoSink.success(res);
-                        createOrderReq.setStatus("CANCELLED");
-                        orderRepository.save(createOrderReq).subscribe();
-                    }
-                } catch (Exception e) {
-                    monoSink.error(e);
-                }
+                AtomicInteger reservedCounter = new AtomicInteger();
+                Mono.just(createOrderReq)
+                        .flatMap(order -> updaterOrderLineEntryStatus(Objects.requireNonNull(invReserveRes), reservedCounter, order))
+                        .doOnSuccess(order -> updateOuterStatus(reservedCounter, createOrderReq, monoSink))
+                        .doOnError(t -> handleError(t, monoSink))
+                        .subscribe();
             }
 
             @Override
-            public void onFailure(Throwable t) {
-                log.warn("Inside Failure");
-                t.printStackTrace();
-                monoSink.error(new Exception(t.getMessage()));
+            public void onFailure(@NullableDecl Throwable t) {
+                handleError(t, monoSink);
             }
         }, executor));
     }
+
+    private void handleError(Throwable t, MonoSink<CreateOrderResponse> monoSink) {
+        log.error("Inside doOnError");
+        monoSink.error(t);
+        t.printStackTrace();
+    }
+
+    private Mono<Order> updaterOrderLineEntryStatus(ReserveInventoryListResponse invReserveRes, AtomicInteger reservedCounter, Order order) {
+
+        return Flux.fromIterable(order.getOrderLineEntries())
+                .flatMap(orderLine -> Mono.justOrEmpty(invReserveRes.getInvResponseList().stream()
+                                .filter(reserveResponse -> reserveResponse.getProductId().equals(orderLine.getSku()))
+                                .findFirst())
+                        .doOnNext(reserveResponse -> {
+                            if (reserveResponse.getSuccess()) {
+                                orderLine.setStatus("RESERVED");
+                                reservedCounter.incrementAndGet();
+                            } else {
+                                orderLine.setStatus("CANCELLED");
+                                orderLine.setCancellationReason("Insufficient Inventory");
+                            }
+                        })).then(Mono.just(order));
+    }
+
+    private void updateOuterStatus(AtomicInteger reservedCounter, Order createOrderReq, MonoSink<CreateOrderResponse> monoSink) {
+        CreateOrderResponse res = new CreateOrderResponse();
+        res.setSellerOrderId(createOrderReq.getSellerOrderId());
+        if (reservedCounter.get() == createOrderReq.getOrderLineEntries().size()) {
+            res.setOrderStatus("CONFIRMED");
+            res.setCancellationReason(null);
+            createOrderReq.setStatus("CONFIRMED");
+        } else {
+            res.setOrderStatus("CANCELLED");
+            res.setCancellationReason("Unable to reserve inventory");
+            createOrderReq.setStatus("CANCELLED");
+        }
+        orderRepository.save(createOrderReq).subscribe();
+        monoSink.success(res);
+    }
+
+
+/*    private void processCreateOrderReq(ReserveInventoryListResponse invReserveRes, AtomicInteger reservedCounter, Order order) {
+
+        for (Order.OrderLine orderLine : order.getOrderLineEntries()) {
+            for (ReserveInventoryResponse response : invReserveRes.getInvResponseList()) {
+                if (orderLine.getSku().equals(response.getProductId())) {
+                    if (response.getSuccess()) {
+                        orderLine.setStatus("RESERVED");
+                        reservedCounter.addAndGet(1);
+                    } else {
+                        orderLine.setStatus("CANCELLED");
+                        orderLine.setCancellationReason("Insufficient Inventory");
+                    }
+                    break;
+                }
+            }
+        }
+    }*/
+
 }
